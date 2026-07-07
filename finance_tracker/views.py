35e9eef3e82cc7f76ea6a4c6aa3b5f18b83f329d
Yerke <yerkebulan@gmail.com>
2026-07-07 14:42:52 +0500

    implemented login and register

--- finance_tracker/views.py
+++ finance_tracker/views.py
@@ -1,5 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import AuthenticationForm
+from django.shortcuts import get_object_or_404
+from .forms import RegisterForm
+from django.contrib.auth.decorators import login_required
 
 def home(request):
-    return HttpResponse("Welcome to the Finance Tracker Home Page!")        
+    return render(request, "home.html")
+
+def register(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("home")
+
+    else:
+        form = RegisterForm()
+
+    return render(request, "register.html", {"form": form})
+
+
+def user_login(request):
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect("home")
+
+    else:
+        form = AuthenticationForm()
+
+    return render(request, "login.html", {"form": form})
+
+
+def user_logout(request):
+    logout(request)
+    return redirect("home")
